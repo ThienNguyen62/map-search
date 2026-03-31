a2fed82f1db7791bd7645aa7c3c94859ecb4596a
NH16479hust <hai.nh2416479@sis.hust.edu.vn>
2026-04-01 01:57:40 +0700

    Update test.py

--- backend/algorithms/test.py
+++ backend/algorithms/test.py
@@ -1,4 +1,4 @@
-from graph import create_munich_graph
+from graph_test import create_munich_graph
 from astar import find_route_with_lines as astar
 from dijkstra import find_route_with_lines as dijkstra
 
